917f794781fc8433ff2a529f131fc7a7ff05b2dd
Enrico Lüdecke <e.luedecke@mailbox.org>
2022-04-04 21:39:09 +0200

    Keep calm and commit

--- src/renderer.ts
+++ src/renderer.ts
@@ -4,3 +4,25 @@
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
+
+const filter: any = {
+    debug: document.querySelector('#filterdebug'),
+    notice: document.querySelector('#filternotice'),
+    info: document.querySelector('#filterinfo'),
+    success: document.querySelector('#filtersuccess'),
+    warning: document.querySelector('#filterwarning'),
+    error: document.querySelector('#filtererror'),
+    emergency: document.querySelector('#filteremergency'),
+    alert: document.querySelector('#filteralert'),    
+    critical: document.querySelector('#filtercritical'),
+};
+
+filter.debug.checked = true;
+filter.notice.checked = true
+filter.info.checked = true
+filter.success.checked = true
+filter.warning.checked = true
+filter.error.checked = true
+filter.emergency.checked = true
+filter.alert.checked = true
+filter.critical.checked = true
