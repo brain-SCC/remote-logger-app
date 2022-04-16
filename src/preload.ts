7da48b2a81510675a18227fd75186c88250f6b75
Enrico Lüdecke <e.luedecke@mailbox.org>
2022-04-16 17:26:43 +0200

    added bs icons

--- src/preload.ts
+++ src/preload.ts
@@ -1,14 +1,28 @@
+
 // All of the Node.js APIs are available in the preload process.
 import { App } from "./RemoteLogger/App";
+import { ConsoleLoggerImpl } from './RemoteLogger/ConsoleLogger';
 import { AppConfig } from "./RemoteLogger/config/AppConfig";
 
 // It has the same sandbox as a Chrome extension.
 window.addEventListener("DOMContentLoaded", () => {
   const output = document.getElementById("remote-logger-output");
-  const appConfig = new AppConfig();
-  const app = new App(output, appConfig, console);
-  app.run();
+  if(output) 
+  {
+    const appConfig = new AppConfig();
+    const logger = new ConsoleLoggerImpl(appConfig.isDebugEnabled? console : undefined);
+    const app = new App(output, appConfig, logger);
+    app.run();
 
+    const clearBtn = document.querySelector('#clear-output');
+    if(clearBtn) {
+      clearBtn.addEventListener('click', () => {
+        output.querySelectorAll('*').forEach(n => n.remove());
+      })
+    } 
+  }
+
+  /*
   const replaceText = (selector: string, text: string) => {
     const element = document.getElementById(selector);
     if (element) {
@@ -21,5 +35,5 @@ window.addEventListener("DOMContentLoaded", () => {
       `${type}-version`,
       process.versions[type as keyof NodeJS.ProcessVersions]
     );
-  }
+  }*/
 });
