cc45d62062c40660fe26ab9c35b74dddbb6beee6
Enrico Lüdecke <e.luedecke@brain-scc.de>
2022-09-05 13:36:40 +0200

    foo

--- src/RemoteLogger/printer/ItemPrinter.ts
+++ src/RemoteLogger/printer/ItemPrinter.ts
@@ -1,7 +1,6 @@
 import { SqlPrettyPrint } from "./SqlPrettyPrint";
 import { StacktracePrettyPrint } from "./StacktracePrettyPrint";
 import { LogLevel, LogEntry } from "../types";
-import { ContextPrettyPrint } from "./ContextPrettyPrint";
 
 const logLevelCssClassNames: Record<LogLevel, string> = {
   debug: "border-secondary text-dark",
@@ -19,76 +18,128 @@ const logLevelCssMap = new Map<string, string>(
 );
 
 export class ItemPrinter {
+  private data: LogEntry;
+  private div: HTMLDivElement;
+
+  constructor() {
+    this.data = {};
+    this.div = document.createElement("div");
+  }
+
   public create(data: LogEntry, hidden: boolean): HTMLDivElement {
-    const div: HTMLDivElement = document.createElement("div");
-    const logLevel = data.level ?? "debug";
-    const message = data.message ?? "no message sent";
-    const dateTime = new Date().toLocaleString();
+    this.data = data;
+    this.div = document.createElement("div");
 
-    let text = "";
-    let context = "";
-    let stacktrace = "";
-    let sql = "";
+    this.setClasslist(hidden);
+    this.appendMessageNode();
+    this.appendTextNode();
+    this.appendSqlNode();
+    this.appendContextNode();
+    this.appendStacktraceNode();
+    this.appendMetaNode();
 
-    if (data.text) {
-      text = `<p>${data.text}</p>`
-    }
+    return this.div;
+  }
 
-    if (data.stacktrace) {
-      stacktrace = `<p>Stacktrace:<br>${StacktracePrettyPrint.format(data.stacktrace)}</p>`
-    }
+  private getLogLevel(): LogLevel {
+    return this.data.level ?? "debug";
+  }
 
-    if (data.sql) {
-      if(logLevel === "alert" || logLevel === "critical") {
-        sql = `<code class="text-white">${SqlPrettyPrint.format(data.sql)}</code>`
-      }
-      else {
-        sql = `<code>${SqlPrettyPrint.format(data.sql)}</code>`
-      }
-    }
-
-    if (data.context) {
-        context = `<p>Context:<br>${ContextPrettyPrint.format(data.context)}</p>`
-    }
-
-    let meta = "";
-
-    if (data.file) {
-      meta += `File: ${data.file}`
-    }
-    if (data.method) {
-      meta += ` Method: ${data.method}`
-    }
-    if (data.line) {
-      meta += ` at line: ${data.line}`
-    }
-
-    meta += ` ${dateTime}`
-
-    div.classList.add(
-      "shadow-sm", 
-      "p-3", 
-      "mb-2", 
-      "rounded", 
-      "border-5", 
-      "border-start", 
+  private setClasslist(hidden: boolean): void {
+    const logLevel = this.getLogLevel();
+    this.div.classList.add(
+      "shadow-sm",
+      "p-3",
+      "mb-2",
+      "rounded",
+      "border-5",
+      "border-start",
       "log-entry",
       `ll-${logLevel}`
-    )
+    );
 
-    if(hidden) {
-      div.classList.add("d-none")
+    if (hidden) {
+      this.div.classList.add("d-none");
     }
 
-    const classNames = logLevelCssMap.get(logLevel) ?? logLevelCssMap.get("debug")
-    classNames?.split(" ").forEach(css => div.classList.add(css))
+    const classNames =
+      logLevelCssMap.get(logLevel) ?? logLevelCssMap.get("debug");
+    classNames?.split(" ").forEach((css) => this.div.classList.add(css));
+  }
 
-    div.innerHTML = `
-        <p>${message}</p>
-        ${text} ${sql} ${context} ${stacktrace}
-        <small class="text-muted">${meta}</small>
-    `;
+  private appendMessageNode(): void {
+    const message = this.data.message ?? "no message sent";
+    const msgNode = document.createElement("p");
+    msgNode.appendChild(document.createTextNode(message));
+    this.div.appendChild(msgNode);
+  }
 
-    return div
+  private appendTextNode(): void {
+    if (this.data.text) {
+      const textNode = document.createElement("p");
+      textNode.appendChild(document.createTextNode(this.data.text));
+      this.div.appendChild(textNode);
+    }
+  }
+
+  private appendSqlNode(): void {
+    if (this.data.sql) {
+      const sqlNode = document.createElement("code");
+      const logLevel = this.getLogLevel();
+      if (logLevel === "alert" || logLevel === "critical") {
+        sqlNode.classList.add("text-white");
+      }
+      sqlNode.innerHTML = SqlPrettyPrint.format(this.data.sql);
+      this.div.appendChild(sqlNode);
+    }
+  }
+
+  private appendContextNode(): void {
+    if (this.data.context && typeof this.data.context == "object") {
+      const contextNode = document.createElement("p");
+      contextNode.appendChild(document.createTextNode("Context:"));
+      contextNode.appendChild(document.createElement("br"));
+      const jsonNode = document.createElement("div");
+      jsonNode.innerHTML = JSON.stringify(this.data.context, null, 4)
+        .replaceAll("  ", "&nbsp;&nbsp;")
+        .replaceAll("\n", "<br>")
+      contextNode.appendChild(jsonNode)
+      this.div.appendChild(contextNode)
+    }
+  }
+
+  private appendStacktraceNode(): void {
+    if (this.data.stacktrace) {
+      const stackNode = document.createElement("p");
+      stackNode.appendChild(document.createTextNode("Stacktrace:"));
+      stackNode.appendChild(document.createElement("br"));
+      this.div.appendChild(stackNode);
+
+      const traceNode = document.createElement("div");
+      traceNode.innerHTML = StacktracePrettyPrint.format(this.data.stacktrace);
+      this.div.appendChild(traceNode);
+    }
+  }
+
+  private appendMetaNode(): void {
+    const metaNode = document.createElement("small");
+    metaNode.classList.add("text-muted");
+
+    let meta = "";
+    if (this.data.file) {
+      meta += `File: ${this.data.file}`;
+    }
+    if (this.data.method) {
+      meta += ` Method: ${this.data.method}`;
+    }
+    if (this.data.line) {
+      meta += ` at line: ${this.data.line}`;
+    }
+
+    const dateTime = new Date().toLocaleString();
+    meta += ` ${dateTime}`;
+
+    metaNode.appendChild(document.createTextNode(meta));
+    this.div.appendChild(metaNode);
   }
 }
